83f089b36b79a5122eed22aeb94e578769561b1d
StaryHroft <demony4321@gmail.com>
2026-05-23 15:59:59 +0300

    init: приложение запускается, БД подключена, таблицы созданы

---
+++ src/main/java/by/staryhroft/userbalance/UserBallanceServiceApplication.java
@@ -0,0 +1,12 @@
+package by.staryhroft.userbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBallanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBallanceServiceApplication.class, args);
+    }
+}
